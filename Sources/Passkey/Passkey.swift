1464378b3b65b98d83111902ed1e2d48744d829a
paulmac <paulanthony@gmetarave.com>
2024-12-30 17:49:45 -0800

    updates

--- Sources/Passkey/Passkey.swift
+++ Sources/Passkey/Passkey.swift
@@ -1,22 +1,26 @@
 import SwiftGodot
 
 // Initialize the Swift extension for Godot
-@_cdecl("swift_entry_point")
-func initSwiftExtension() {
-    SwiftGodot.initialize(types: [Passkey.self])
-}
+#initSwiftExtension(
+    cdecl: "swift_entry_point",
+    types: [
+        Passkey.self
+    ]
+)
+
 
 @Godot
 class Passkey: RefCounted, PasskeyDelegate {
     private let nativePasskey = NativePasskey()
 
     // Define signals for Godot to listen to
-    #signal("sign_in_completed", arguments: ["responseJson": String.self])
     #signal("sign_in_error", arguments: ["errorMessage": String.self])
+    #signal("sign_in_completed", arguments: ["responseJson": String.self])
+    
     #signal("create_passkey_completed", arguments: ["responseJson": String.self])
     #signal("create_passkey_error", arguments: ["errorMessage": String.self])
 
-    required init() {
+    required init() {ƒ
         super.init()
         nativePasskey.delegate = self
     }
@@ -36,10 +40,12 @@ class Passkey: RefCounted, PasskeyDelegate {
         nativePasskey.createPasskey(requestJson: requestJson)
     }
 
+
     // MARK: - PasskeyDelegate
     func didCompleteWithSuccess(_ responseJson: String) {
         emit(signal: Passkey.sign_in_completed, responseJson)
     }
+
 
     func didEncounterError(_ errorMessage: String) {
         emit(signal: Passkey.sign_in_error, errorMessage)
